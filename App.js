c127af8342d5c3d0851a8c51c97c79b4de8d482d
choiby777 <choiby777@gmail.com>
2018-04-24 00:30:08 +0900

    중간 commit

--- App.js
+++ App.js
@@ -1,21 +1,93 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, Platform, TextInput, ScrollView} from 'react-native';
+import Todo from './ToDo'
+
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state = {
+    newTodo: ""
+  }
+  
   render() {
+    const { newTodo } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Test !!!</Text>
+        <StatusBar barStyle="light-content"></StatusBar>
+        <Text style={styles.titleText}>Todo App</Text>
+        <View style={styles.card}>
+          <TextInput 
+            style={styles.inputText} 
+            underlineColorAndroid = 'transparent'
+            value={newTodo}
+            onChangeText={this._controllNewTodo}
+            placeholder={"New To Do"}
+            placeholderTextColor = '#999' 
+            returnKeyType = "done"
+            autoCorrect = {false}>
+          </TextInput>
+          <ScrollView>
+            <Todo itemText = '1' />
+            <Todo itemText = '2'/>
+            <Todo itemText = '3'/>            
+          </ScrollView>
+        </View>
       </View>
     );
+  }
+
+  _controllNewTodo = text =>{
+    this.setState({
+      newTodo : text
+    });
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff7777',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
+
+  titleText:{
+    color: 'white',
+    fontSize : 30,
+    fontWeight: "500",
+    marginTop : 50,
+  },
+ 
+  card:{
+    flex: 1,
+    backgroundColor: '#ffffca',
+    width: width - 30,  
+    //borderRadius: 10,
+    borderTopLeftRadius : 10,
+    borderTopRightRadius : 10,
+    //elevation: 10
+    ...Platform.select({
+      ios:{
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity : 0.5,
+        shdowRadius:5,
+        shadowOffset:{
+          height: -1,
+          width:0
+        }
+      },
+      android:{
+        elevation: 3
+      }
+    })
+  },
+
+  inputText :{
+    fontSize : 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth : 1,
+    padding : 25, 
+  },
+
 });
---
+++ ToDo.js
@@ -0,0 +1,32 @@
+import React , {Component} from "react";
+import { View , Text, StyleSheet, Dimensions} from "react-native";
+
+const { height, width } = Dimensions.get("window");
+
+export default class ToDo extends Component {
+    state = { 
+        isEditing: false
+    }
+
+    constructor(props){
+        super(props);        
+    }
+
+    render() {
+        const { itemText } = this.state;
+        return (
+            <View style={styles.container}>
+                <Text>Item : {this.props.itemText}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container :{
+        flex: 1,
+        backgroundColor: 'red',
+        width : width - 50,
+        
+    },
+})
