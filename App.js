9d1dfcbe1261443af69ac39eedfe30ad6e1816ac
choiby777 <choiby777@gmail.com>
2018-04-25 23:42:11 +0900

    코드 정렬

--- App.js
+++ App.js
@@ -1,93 +1,101 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Dimensions, Platform, TextInput, ScrollView} from 'react-native';
-import Todo from './ToDo'
+import React from "react"
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Dimensions,
+  Platform,
+  TextInput,
+  ScrollView
+} from "react-native"
+import Todo from "./ToDo"
 
-const { height, width } = Dimensions.get("window");
+const { height, width } = Dimensions.get("window")
 
 export default class App extends React.Component {
   state = {
     newTodo: ""
   }
-  
+
   render() {
-    const { newTodo } = this.state;
+    const { newTodo } = this.state
 
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content"></StatusBar>
+        <StatusBar barStyle="light-content" />
         <Text style={styles.titleText}>Todo App</Text>
         <View style={styles.card}>
-          <TextInput 
-            style={styles.inputText} 
-            underlineColorAndroid = 'transparent'
+          <TextInput
+            style={styles.inputText}
+            underlineColorAndroid="transparent"
             value={newTodo}
             onChangeText={this._controllNewTodo}
             placeholder={"New To Do"}
-            placeholderTextColor = '#999' 
-            returnKeyType = "done"
-            autoCorrect = {false}>
-          </TextInput>
+            placeholderTextColor="#999"
+            returnKeyType="done"
+            autoCorrect={false}
+          />
           <ScrollView>
-            <Todo itemText = '1' />
-            <Todo itemText = '2'/>
-            <Todo itemText = '3'/>            
+            <Todo itemText="1" />
+            <Todo itemText="2" />
+            <Todo itemText="3" />
           </ScrollView>
         </View>
       </View>
-    );
+    )
   }
 
-  _controllNewTodo = text =>{
+  _controllNewTodo = text => {
     this.setState({
-      newTodo : text
-    });
+      newTodo: text
+    })
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff7777',
-    alignItems: 'center',
+    backgroundColor: "#ff7777",
+    alignItems: "center"
     //justifyContent: 'center',
   },
 
-  titleText:{
-    color: 'white',
-    fontSize : 30,
+  titleText: {
+    color: "white",
+    fontSize: 30,
     fontWeight: "500",
-    marginTop : 50,
+    marginTop: 50
   },
- 
-  card:{
+
+  card: {
     flex: 1,
-    backgroundColor: '#ffffca',
-    width: width - 30,  
+    backgroundColor: "#ffffca",
+    width: width - 30,
     //borderRadius: 10,
-    borderTopLeftRadius : 10,
-    borderTopRightRadius : 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
     //elevation: 10
     ...Platform.select({
-      ios:{
+      ios: {
         shadowColor: "rgb(50,50,50)",
-        shadowOpacity : 0.5,
-        shdowRadius:5,
-        shadowOffset:{
+        shadowOpacity: 0.5,
+        shdowRadius: 5,
+        shadowOffset: {
           height: -1,
-          width:0
+          width: 0
         }
       },
-      android:{
+      android: {
         elevation: 3
       }
     })
   },
 
-  inputText :{
-    fontSize : 20,
+  inputText: {
+    fontSize: 20,
     borderBottomColor: "#bbb",
-    borderBottomWidth : 1,
-    padding : 25, 
-  },
-
-});
+    borderBottomWidth: 1,
+    padding: 25
+  }
+})
--- ToDo.js
+++ ToDo.js
@@ -1,32 +1,31 @@
-import React , {Component} from "react";
-import { View , Text, StyleSheet, Dimensions} from "react-native";
+import React, { Component } from "react"
+import { View, Text, StyleSheet, Dimensions } from "react-native"
 
-const { height, width } = Dimensions.get("window");
+const { height, width } = Dimensions.get("window")
 
 export default class ToDo extends Component {
-    state = { 
-        isEditing: false
-    }
+  state = {
+    isEditing: false
+  }
 
-    constructor(props){
-        super(props);        
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    render() {
-        const { itemText } = this.state;
-        return (
-            <View style={styles.container}>
-                <Text>Item : {this.props.itemText}</Text>
-            </View>
-        );
-    }
+  render() {
+    const { itemText } = this.state
+    return (
+      <View style={styles.container}>
+        <Text>Item : {this.props.itemText}</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container :{
-        flex: 1,
-        backgroundColor: 'red',
-        width : width - 50,
-        
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    width: width - 50
+  }
 })
